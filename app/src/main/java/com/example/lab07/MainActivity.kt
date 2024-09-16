a56efe557e1f0ee35553032c174c01232563fee1
Y-ostin <yostin.arequipa@tecsup.edu.pe>
2024-09-16 17:15:05 -0500

    chore: create database

--- app/src/main/java/com/example/lab07/MainActivity.kt
+++ app/src/main/java/com/example/lab07/MainActivity.kt
@@ -19,12 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab07Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
